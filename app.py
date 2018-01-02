a78004e95290491b207e503eff81709d72bdbc0a
edj36 <edj36@cornell.edu>
2018-01-01 22:41:32 -0500

    getting rid of single page scrolling stuff, moving to multiple pages

--- app.py
+++ app.py
@@ -17,21 +17,37 @@ def read_calendar():
     return ""
 
 @app.route("/")
-def hello():
+def index_page():
     """
     Return template for index.html
     """
 
     return render_template("index.html")
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route("/request")
+def request_page():
+    """
+    Return template for request.html
+    """
+
+    return render_template("request.html")
+
+@app.route("/admin")
+def admin_page():
+    """
+    Return template for index.html
+    """
+
+    return render_template("admin.html")
+
+@app.route('/request', methods=['POST'])
+def request_page_post():
 
     input_requester_email = request.form['input_requester_email']
     print(input_requester_email)
     # send some kind of alert to display here
     write_calendar(input_requester_email)
-    return render_template("index.html")
+    return render_template("request.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
